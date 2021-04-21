e4ad6030ee3ca8bf8e758ad32938212f7982ba78
EyaBarhoumi <60940615+EyaBarhoumi@users.noreply.github.com>
2021-04-21 03:19:36 +0100

    Order

---
+++ pidevShoppyTounsi/Models/OrderLine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pidevShoppyTounsi.Models
+{
+    public class OrderLine
+    {
+        public int orderLineId { get; set; }
+        public Product product { get; set; }
+        public int quantity { get; set; }
+        public double price { get; set; }
+        public bool confirmed { get; set; }
+        public Shoppingcart shoppingCart { get; set; }
+    }
+}
